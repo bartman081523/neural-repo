3c5c441e03f30cd8041dfe2d1556bc235762a041
chlorophyll-zz <chlorophyll273@gmail.com>
2021-05-10 12:38:59 +0200

    3<

--- install.sh
+++
@@ -1,2 +0,0 @@
-pip3 install -r requirements.txt
-!wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
--- run.sh
+++
@@ -1,1 +0,0 @@
-python3 neural_style.py --content ./inputimage/IMG_20210216_115328_2.jpg --styles ./styles/isa-p7.jpg --output output1.png --iterations 1000
